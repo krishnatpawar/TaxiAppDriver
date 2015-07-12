146560716171dbc560f47e6e5578251c6086df0a
koti <koduri.2013@gmail.com>
2015-07-12 16:26:08 +0530

    login,register and forget pwd functionality

---
+++ app/src/main/java/com/taxi/driver/taxiappdriver/activity/UpdateActivity.java
@@ -0,0 +1,17 @@
+package com.taxi.driver.taxiappdriver.activity;
+
+import android.os.Bundle;
+
+import com.taxi.driver.taxiappdriver.R;
+
+/**
+ * Created by koti on 7/12/2015.
+ */
+public class UpdateActivity extends AbstractTaxiDriverActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.profile_update);
+    }
+}
